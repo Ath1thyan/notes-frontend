e4028ca99666d2ce6203566aeda6dbc57d139b8f
Ath1thyan <athitasks@gmail.com>
2024-07-01 03:28:09 +0530

    Working on cards

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ProfileInfo from '../cards/ProfileInfo';
+import { useNavigate } from 'react-router-dom';
+import SearchBar from '../searchbar/SearchBar';
 
 const Navbar = () => {
-  return (
-    <div className='bg-white flex items-center justify-between px-6 py-2 drop-shadow'>
-        <h1 className='text-xl font-medium text-black py-2'>Travels</h1>
-    </div>
-  )
+
+	const [searchQuery, setSearchQuery] = useState('');
+
+	const navigate = useNavigate()
+
+	const onLogout = () => {
+		navigate("/login");
+	}
+
+	const handleSearch = () => {
+        navigate(`/search/${searchQuery}`)
+    }
+
+	const onClearSearch = () => {
+        setSearchQuery('')
+    }
+
+	return (
+		<div className='bg-white flex items-center justify-between px-6 py-2 drop-shadow'>
+			<h1 className='text-xl font-medium text-black py-2'>Travels</h1>
+
+			<SearchBar 
+				value={searchQuery} 
+				onChange={({ target }) => {
+					setSearchQuery(target.value)
+				}}
+				handleSearch={handleSearch}
+				onClearSearch={onClearSearch} 
+			/>
+
+			<ProfileInfo onLogout={onLogout} />
+		</div>
+	)
 }
 
 export default Navbar;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import Navbar from '../../components/navbar/Navbar';
+import TripCard from '../../components/cards/TripCard';
+import { MdAdd } from "react-icons/md";
+import AddEditTrips from './AddEditTrips';
 
 const Home = () => {
   return (
     <div>
-      Home
+      <Navbar />
+
+      <div className='container mx-auto'>
+        <div className='grid grid-cols-3 gap-4 mt-8'>
+          <TripCard 
+            title="Trip to Ooty on 6th July"
+            date="2nd Jul 2024"
+            content="Trip to Ooty on 6th July Trip to Ooty on 6th July Trip to Ooty on 6th July"
+            tags="#1DayTrip"
+            isBookMarked={false}
+            onEdit={() => {}}
+            onDelete={() => {}}
+            onBookMarkedTrip={() => {}}
+          />
+        </div>
+      </div>
+
+      <button className='w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10' onClick={() => {}}>
+        <MdAdd className="text-[32px] text-white" />
+      </button>
+
+      <AddEditTrips />
     </div>
   )
 }
