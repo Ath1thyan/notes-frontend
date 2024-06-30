eec5bfc9adeb26d340b2c36ffd7ac90930f694f4
Ath1thyan <athitasks@gmail.com>
2024-07-01 01:59:07 +0530

    Login, signup UI completed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <div className='bg-white flex items-center justify-between px-6 py-2 drop-shadow'>
+        <h1 className='text-xl font-medium text-black py-2'>Travels</h1>
     </div>
   )
 }
--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,8 +1,63 @@
+import React, { useState } from 'react'
+import { Link } from "react-router-dom";
+import Navbar from "../../components/navbar/Navbar";
+import PasswordInput from "../../components/input/PasswordInput";
+import { validateEmail } from "../../utils/helper";
 
 const Signup = () => {
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleSignup = async (e) => {
+    e.preventDefault();
+
+    if (!name) {
+      setError("Name is required.");
+      return;
+    }
+
+    if (!validateEmail(email)) {
+      setError("Invalid email address.");
+      return;
+    }
+
+    if (!password) {
+      setError("Password is required.");
+      return;
+    }
+
+    setError("")
+  }
+
   return (
     <div>
-      Signup
+
+      <Navbar />
+
+      <div className="flex items-center justify-center mt-28">
+        <div className="w-96 border rounded bg-white px-7 py-10">
+
+          <form onSubmit={handleSignup}>
+            <h4 className="text-2xl mb-7">SignUp</h4>
+
+            <input type="text" placeholder="Name" className="input-box" value={name} onChange={(e) => setName(e.target.value)} />
+            <input type="text" placeholder="Email" className="input-box" value={email} onChange={(e) => setEmail(e.target.value)} />
+
+            <PasswordInput value={password} onChange={(e) => setPassword(e.target.value)} />
+
+            {error && <p className="error-message">{error}</p>}
+
+            <button type="submit" className="btn-primary">Create Account</button>
+
+            <p className="text-center text-sm mt-4">Already have an account? <Link to="/login" className="font-medium text-primary underline">Login</Link></p>
+
+          </form>
+        </div>
+      </div>
+
     </div>
   )
 }
