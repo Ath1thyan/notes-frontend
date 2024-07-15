bdbafe269a57caf91f1d545dfc56118ac05b34f1
Ath1thyan <athitasks@gmail.com>
2024-07-15 17:54:53 +0530

    changes made

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ const routes = (
     <Routes>
       <Route path="/dashboard" exact element={<Home />} />
       <Route path="/login" exact element={<Login />} />
+      <Route path="/" exact element={<Login />} />
       <Route path="/signup" exact element={<Signup />} />
     </Routes>
   </Router>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -27,7 +27,7 @@ const Navbar = ({userInfo, onSearchTrip, handleClearSearch }) => {
 
 	return (
 		<div className='bg-white flex items-center justify-between px-6 py-2 drop-shadow'>
-			<h1 className='text-xl font-medium text-black py-2'>Travels</h1>
+			<h1 className='text-xl font-medium text-black py-2'>Notes</h1>
 
 			<SearchBar 
 				value={searchQuery} 
